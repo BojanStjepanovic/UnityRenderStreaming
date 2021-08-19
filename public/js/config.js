119bef81ba0b9f7ec654c3418eabb7aaa9b8921c
Bojan Stjepanovic <bojan.stjepanovic@hotmail.com>
2021-08-19 20:24:05 +0200

    config

--- public/js/config.js
+++ public/js/config.js
@@ -1,12 +1,21 @@
 export async function getServerConfig() {
-  const protocolEndPoint = location.origin + '/config';
+  const protocolEndPoint = location.origin + "/config";
   const createResponse = await fetch(protocolEndPoint);
   return await createResponse.json();
 }
 
 export function getRTCConfiguration() {
   let config = {};
-  config.sdpSemantics = 'unified-plan';
-  config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
+  config.sdpSemantics = "unified-plan";
+  config.iceServers = [
+    {
+      urls: ["stun:stun.l.google.com:19302"],
+    },
+    {
+      urls: ["turn:192.158.29.39:3478?transport=tcp"],
+      username: "1379330808",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA",
+    },
+  ];
   return config;
 }
